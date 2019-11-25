2d75379e1521012c31e20f352f738700bf1d028b
Angel Reyes-Alfonso <areyesalfonso@japan.udev.bia-boeing.com>
2019-11-25 11:32:00 -0500

    Last commit before deploy

--- github-finder/src/components/pages/Home.js
+++ github-finder/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import Search from '../users/Search';
-import Users from '../users/User';
+import Users from '../users/Users';
 
 const Home = () => (
 	<Fragment>
--- github-finder/src/context/github/GithubState.js
+++ github-finder/src/context/github/GithubState.js
@@ -2,7 +2,6 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
-
 import {
 	SEARCH_USERS,
 	SET_LOADING,
@@ -10,6 +9,17 @@ import {
 	GET_USER,
 	GET_REPOS
 } from '../types';
+
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+	githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+	githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+	githubClientId = process.env.GITHUB_CLIENT_ID;
+	githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
 
 const GithubState = props => {
 	const initialState = {
@@ -26,7 +36,7 @@ const GithubState = props => {
 		setLoading();
 
 		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
@@ -40,7 +50,7 @@ const GithubState = props => {
 		setLoading();
 
 		const res = await axios.get(
-			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
@@ -54,7 +64,7 @@ const GithubState = props => {
 		setLoading();
 
 		const res = await axios.get(
-			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
